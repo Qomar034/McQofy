773d70cc8eb3a2655e4188575864a01ae0e6a041
Qomar034 <nurqomarudin034@gmail.com>
2023-01-18 00:27:24 +0700

    Initiate Models and Migrations

---
+++ Server/services/app/migrations/20230117172144-create-promos.js
@@ -0,0 +1,43 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Promos', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING
+      },
+      image: {
+        type: Sequelize.STRING
+      },
+      caption: {
+        type: Sequelize.TEXT
+      },
+      description: {
+        type: Sequelize.TEXT
+      },
+      expired: {
+        type: Sequelize.STRING
+      },
+      UserId: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Promos');
+  }
+};
---
+++ Server/services/app/models/promos.js
@@ -0,0 +1,28 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Promos extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Promos.init({
+    name: DataTypes.STRING,
+    image: DataTypes.STRING,
+    caption: DataTypes.TEXT,
+    description: DataTypes.TEXT,
+    expired: DataTypes.STRING,
+    UserId: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'Promos',
+  });
+  return Promos;
+};
